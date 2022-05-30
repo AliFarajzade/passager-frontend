94b4b2036b8f21c939fce57d4483ff478b72a150
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-30 01:52:05 -0400

    Add navbar to layout component.

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,11 +1,14 @@
+import Navbar from '../navbar/navbar.component'
+
 type IProps = {
     children: React.ReactNode
 }
 
 const Layout: React.FC<IProps> = ({ children }) => (
-    <>
+    <div className="max-w-screen-2xlxl mx-auto">
+        <Navbar />
         <main>{children}</main>
-    </>
+    </div>
 )
 
 export default Layout
