4116603ce0a116aae194ea932894c02ed10a4668
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-30 07:14:11 -0400

    Add home hero component to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
+import HomeHero from '../components/home/home-hero.component'
 
 const Home: NextPage = () => {
-    return null
+    return (
+        <div className="p-8">
+            <HomeHero />
+        </div>
+    )
 }
 
 export default Home
