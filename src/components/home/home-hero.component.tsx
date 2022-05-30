df3e6ac9686470834ba628af7d99dee23ba06256
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-30 07:13:55 -0400

    Create home hero component.

---
+++ src/components/home/home-hero.component.tsx
@@ -0,0 +1,14 @@
+import styles from './home-hero.module.scss'
+
+const HomeHero = () => {
+    return (
+        <header className={styles.hero}>
+            <h1 className="text-6xl  tracking-[.45em]">OUTDOORS</h1>
+            <h4 className="text-2xl font-semibold tracking-[.4em]">
+                IS WHERE LIFE HAPPENS
+            </h4>
+        </header>
+    )
+}
+
+export default HomeHero
