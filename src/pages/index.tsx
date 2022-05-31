a871989633d9319152be0dcfd1129f8fbee84801
AliFarajzade <alifarajzade1@yahoo.com>
2022-05-31 05:00:21 -0400

    Fix color in selection.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import HomeHero from '../components/home/hero/home-hero.component'
 
 const Home: NextPage = () => {
     return (
-        <div className="lg:p-8 selection:bg-lightGreenAlpha">
+        <div className="lg:p-8 selection:bg-lightGreenAlpha selection:text-white">
             <div className="bg-gray-100">
                 <HomeHero />
                 <main className="px-10"></main>
