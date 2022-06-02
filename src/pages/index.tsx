27f6741699863ff9c8a789184d9b023f65713040
AliFarajzade <alifarajzade1@yahoo.com>
2022-06-02 06:51:09 -0400

    Add tour info component to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from 'next'
 import HomeHero from '../components/home/hero/home-hero.component'
+import ToursInfo from '../components/home/tours-info/tours-info.component'
 
 const Home: NextPage = () => {
     return (
         <div className="lg:p-8 selection:bg-lightGreenAlpha selection:text-white">
             <div className="bg-gray-100">
                 <HomeHero />
-                <main className="px-10"></main>
+                <main className="px-10">
+                    <ToursInfo />
+                </main>
             </div>
         </div>
     )
