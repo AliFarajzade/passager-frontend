742d7c595ad019f6a30c4f971cd609bcfaca0c06
AliFarajzade <alifarajzade1@yahoo.com>
2022-06-03 09:52:45 -0400

    Create most populat tour component and add most popular tour card component to it.

---
+++ src/components/home/most-popular-tours/most-popular-tours.component.tsx
@@ -0,0 +1,62 @@
+import MostPopularToursCard from '../most-popular-tours-card/most-popular-tours-card.component'
+
+const cardsData = [
+    {
+        name: 'The Sea Explorer',
+        details: [
+            '3 days tours',
+            'Up to 30 people',
+            '2 tour guides',
+            'Sleep in cozy hotels',
+            'Difficulty: easy',
+        ],
+        price: 297,
+    },
+    {
+        name: 'The Forest Hiker',
+        details: [
+            '7 days tours',
+            'Up to 40 people',
+            '6 tour guides',
+            'Sleep in provided tents',
+            'Difficulty: medium',
+        ],
+        price: 497,
+    },
+    {
+        name: 'The Snow Adventurer',
+        details: [
+            '5 days tours',
+            'Up to 15 people',
+            '3 tour guides',
+            'Sleep in provided tents',
+            'Difficulty: hard',
+        ],
+
+        price: 897,
+    },
+]
+
+const MostPopularTours: React.FC = () => {
+    return (
+        <section className="py-32 px-10 ">
+            <h1 className="gradient-title mb-12">Most Popular Tours</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center lg:h-[36rem] items-center gap-6 xl:gap-20">
+                {cardsData.map((cardData, index) => (
+                    <MostPopularToursCard
+                        key={cardData.name}
+                        cardData={cardData}
+                        index={index + 1}
+                    />
+                ))}
+            </div>
+            <div className="text-center mx-auto pt-10">
+                <button className="btn btn-primary shadow-lg w-64 h-8 text-lg bg-lightGreenAlpha text-white rounded-full">
+                    Explore Tours
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default MostPopularTours
