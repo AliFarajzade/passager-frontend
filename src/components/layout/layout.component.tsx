5e7893cb304238b46705ab124bfce793e04d27f1
AliFarajzade <alifarajzade1@yahoo.com>
2022-06-03 11:12:33 -0400

    Add footer component to layout component.

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,3 +1,4 @@
+import Footer from '../footer/footer.component'
 import AuthModal from '../modal/auth-modal.component'
 import Navbar from '../navbar/navbar.component'
 
@@ -9,6 +10,7 @@ const Layout: React.FC<IProps> = ({ children }) => (
     <div className="max-w-screen-2xl mx-auto">
         <Navbar />
         <main>{children}</main>
+        <Footer />
         <AuthModal />
     </div>
 )
