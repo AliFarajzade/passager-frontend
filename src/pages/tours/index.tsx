02dd11e039395a04bc3fa0fb289384e5c3dabdb3
AliFarajzade <alifarajzade1@yahoo.com>
2022-06-04 04:55:34 -0400

    Create tours page.

---
+++ src/pages/tours/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
+
+const ToursPage: NextPage = () => {
+    return <ToursPageSlider />
+}
+
+export default ToursPage
