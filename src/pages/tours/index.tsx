12f835c02bb1eba98fda7eebf9d010e21c9b4750
AliFarajzade <alifarajzade1@yahoo.com>
2022-06-08 10:30:40 -0400

    Add tours section to tour page.

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from 'next'
+import ToursPageLayout from '../../components/layout/tours-page-layout.component'
+import TourCard from '../../components/tour-card/tour-card.component'
 import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
 
 const ToursPage: NextPage = () => {
-    return <ToursPageSlider />
+    return (
+        <>
+            <ToursPageSlider />
+            <ToursPageLayout>
+                <div className="bg-blue-300">Sidebar</div>
+                <section className="space-y-10 p-5">
+                    <TourCard />
+                </section>
+            </ToursPageLayout>
+        </>
+    )
 }
 
 export default ToursPage
