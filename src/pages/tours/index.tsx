3dc49a908adfe8d5a10a4d82531728c7f8636e83
Ali <alifarajzade1@yahoo.com>
2022-06-18 21:13:50 +0430

    Add price range component to tour page.

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import DatePicker from '../../components/date-picker/date-picker.component'
 import ToursPageLayout from '../../components/layout/tours-page-layout.component'
+import PriceRange from '../../components/price-range/price-range.component'
 import TourCard from '../../components/tour-card/tour-card.component'
 import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
 
@@ -9,13 +10,17 @@ const ToursPage: NextPage = () => {
         <>
             <ToursPageSlider />
             <ToursPageLayout>
-                <section className="mt-5 rounded-md overflow-hidden sticky top-2 h-full">
+                <section className="mt-5 rounded-md overflow-hidden sticky top-2 h-full bg-gray-100">
                     <DatePicker />
-                    {/* <PriceRange /> */}
+                    <PriceRange />
                     {/* <SelectDuration /> */}
                     {/* <RatingPicker /> */}
                 </section>
                 <section className="space-y-10 p-5">
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
                     <TourCard />
                 </section>
             </ToursPageLayout>
