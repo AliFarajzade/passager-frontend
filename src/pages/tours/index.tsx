550db27c57aaf39430a29226ff64845bd6bda67f
Ali <alifarajzade1@yahoo.com>
2022-06-18 20:50:53 +0430

    Add date picker component to tour page.

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import DatePicker from '../../components/date-picker/date-picker.component'
 import ToursPageLayout from '../../components/layout/tours-page-layout.component'
 import TourCard from '../../components/tour-card/tour-card.component'
 import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
@@ -8,7 +9,12 @@ const ToursPage: NextPage = () => {
         <>
             <ToursPageSlider />
             <ToursPageLayout>
-                <div className="bg-blue-300">Sidebar</div>
+                <section className="mt-5 rounded-md overflow-hidden sticky top-2">
+                    <DatePicker />
+                    {/* <PriceRange /> */}
+                    {/* <SelectDuration /> */}
+                    {/* <RatingPicker /> */}
+                </section>
                 <section className="space-y-10 p-5">
                     <TourCard />
                 </section>
