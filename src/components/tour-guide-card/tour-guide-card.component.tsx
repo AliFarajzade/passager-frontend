a883560da7632a142ddf6fef8c3cd5e5750e8e8b
Ali <alifarajzade1@yahoo.com>
2022-06-19 19:44:58 +0430

    Add instagram link.

--- src/components/tour-guide-card/tour-guide-card.component.tsx
+++ src/components/tour-guide-card/tour-guide-card.component.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { FaInstagram } from 'react-icons/fa'
 
 interface IProps {
     tourGuide: {
@@ -11,7 +12,7 @@ interface IProps {
 const TourGuideCard: React.FC<IProps> = ({ tourGuide }) => {
     return (
         <div
-            className="max-h-80 border-[1px] border-lightGreenAlpha rounded-lg shadow-lg flex flex-col items-center
+            className="max-h-[400px] border-[1px] border-lightGreenAlpha rounded-lg shadow-lg flex flex-col items-center
         py-6 px-4 text-center"
         >
             <div className="w-24 mask mask-squircle">
@@ -22,8 +23,17 @@ const TourGuideCard: React.FC<IProps> = ({ tourGuide }) => {
                     alt={tourGuide.name}
                 />
             </div>
-            <h2 className="py-4 text-xl font-bold">{tourGuide.name}</h2>
-            <p className="text-gray-600 ">{tourGuide.description}</p>
+            <div className="mb-3">
+                <h2 className="py-4 text-xl font-bold">{tourGuide.name}</h2>
+                <p className="text-gray-600 ">{tourGuide.description}</p>
+            </div>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                <FaInstagram
+                    size="2.7em"
+                    className="text-gray-400 hover:text-gray-800"
+                    cursor="pointer"
+                />
+            </a>
         </div>
     )
 }
