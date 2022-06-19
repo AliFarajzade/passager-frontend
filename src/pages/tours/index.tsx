93433b8436a4bb9a4468b43fc681da6d82e8eabf
Ali <alifarajzade1@yahoo.com>
2022-06-19 09:26:23 +0430

    Add rate picker component to tour page.

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import DatePicker from '../../components/date-picker/date-picker.component'
 import ToursPageLayout from '../../components/layout/tours-page-layout.component'
 import PriceRange from '../../components/price-range/price-range.component'
+import RatingPicker from '../../components/rating-picker/rating-picker.component'
 import SelectDuration from '../../components/select-duration/select-duration.component'
 import TourCard from '../../components/tour-card/tour-card.component'
 import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
@@ -15,7 +16,7 @@ const ToursPage: NextPage = () => {
                     <DatePicker />
                     <PriceRange />
                     <SelectDuration />
-                    {/* <RatingPicker /> */}
+                    <RatingPicker />
                 </section>
                 <section className="space-y-10 p-5">
                     <TourCard />
