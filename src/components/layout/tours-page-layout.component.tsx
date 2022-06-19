401cac95f2a7faaa783eadf003c3597737577786
Ali <alifarajzade1@yahoo.com>
2022-06-19 11:21:36 +0430

    Make component adapt to certain styles.

--- src/components/layout/tours-page-layout.component.tsx
+++ src/components/layout/tours-page-layout.component.tsx
@@ -1,9 +1,14 @@
 interface IProps {
     children: React.ReactNode[]
+    rowReverse?: boolean
 }
-const ToursPageLayout: React.FC<IProps> = ({ children }) => {
+const ToursPageLayout: React.FC<IProps> = ({ children, rowReverse }) => {
     return (
-        <main className="flex gap-5 justify-between  p-5">
+        <main
+            className={`flex gap-5 justify-between p-5 ${
+                rowReverse && 'flex-row-reverse'
+            }`}
+        >
             <section className="hidden lg:w-[337px] xl:block xl:shrink-0">
                 {children[0]}
             </section>
