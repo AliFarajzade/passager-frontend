94454c6446e91ed50cf0beafa99317a160371134
Ali <alifarajzade1@yahoo.com>
2022-06-19 15:59:29 +0430

    Create tour guides component and add tour guide card to it.

---
+++ src/components/tour-guides/tour-guides.component.tsx
@@ -0,0 +1,24 @@
+import TourGuideCard from '../tour-guide-card/tour-guide-card.component'
+
+interface IProps {
+    tourGuides: {
+        name: string
+        photo: string
+        description: string
+    }[]
+}
+
+const TourGuides: React.FC<IProps> = ({ tourGuides }) => {
+    return (
+        <div className="space-y-6">
+            <div className="text-center text-xl font-semibold bg-white shadow-md rounded-md py-5">
+                Your Guides:
+            </div>
+            {tourGuides.map(guide => (
+                <TourGuideCard tourGuide={guide} key={guide.name} />
+            ))}
+        </div>
+    )
+}
+
+export default TourGuides
