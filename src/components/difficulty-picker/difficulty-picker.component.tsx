6801ffb6c2c18079f09785a8331bb93defec8aef
Ali <alifarajzade1@yahoo.com>
2022-06-19 09:42:26 +0430

    Fix some stylings.

--- src/components/difficulty-picker/difficulty-picker.component.tsx
+++ src/components/difficulty-picker/difficulty-picker.component.tsx
@@ -3,21 +3,21 @@ const DifficultyPicker: React.FC = () => {
         <div className="p-4">
             <h2 className="text-lg py-4 font-semibold mb-3">Difficulty</h2>
             <div className="flex flex-col gap-5">
-                <div className="flex items-center gap-4 cursor-pointer">
+                <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
                         type="checkbox"
                         className="checkbox checkbox-primary"
                     />
                     Easy
                 </div>
-                <div className="flex items-center gap-4 cursor-pointer">
+                <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
                         type="checkbox"
                         className="checkbox checkbox-primary"
                     />
                     Medium
                 </div>
-                <div className="flex items-center gap-4 cursor-pointer">
+                <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
                         type="checkbox"
                         className="checkbox checkbox-primary"
