509657d43e7ffad80665fa26df16006889e0d714
Ali <alifarajzade1@yahoo.com>
2022-06-19 10:19:55 +0430

    Add filtering component to tour page.

--- src/pages/tours/index.tsx
+++ src/pages/tours/index.tsx
@@ -5,6 +5,7 @@ import ToursPageLayout from '../../components/layout/tours-page-layout.component
 import PriceRange from '../../components/price-range/price-range.component'
 import RatingPicker from '../../components/rating-picker/rating-picker.component'
 import SelectDuration from '../../components/select-duration/select-duration.component'
+import SmallerScreenFilter from '../../components/small-screen-filter/small-screen-filter.component'
 import TourCard from '../../components/tour-card/tour-card.component'
 import ToursPageSlider from '../../components/tours-page-slider/tours-page-slider.component'
 
@@ -12,6 +13,7 @@ const ToursPage: NextPage = () => {
     return (
         <>
             <ToursPageSlider />
+            <SmallerScreenFilter />
             <ToursPageLayout>
                 <section className="mb-5 rounded-md overflow-hidden bg-gray-100 pb-4">
                     <DatePicker />
