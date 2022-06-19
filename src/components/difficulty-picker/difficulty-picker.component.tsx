8413b62fa0502060bd7a201a468f3a38b6560a89
Ali <alifarajzade1@yahoo.com>
2022-06-19 09:40:16 +0430

    Create difficulty picker component.

---
+++ src/components/difficulty-picker/difficulty-picker.component.tsx
@@ -0,0 +1,32 @@
+const DifficultyPicker: React.FC = () => {
+    return (
+        <div className="p-4">
+            <h2 className="text-lg py-4 font-semibold mb-3">Difficulty</h2>
+            <div className="flex flex-col gap-5">
+                <div className="flex items-center gap-4 cursor-pointer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-primary"
+                    />
+                    Easy
+                </div>
+                <div className="flex items-center gap-4 cursor-pointer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-primary"
+                    />
+                    Medium
+                </div>
+                <div className="flex items-center gap-4 cursor-pointer">
+                    <input
+                        type="checkbox"
+                        className="checkbox checkbox-primary"
+                    />
+                    Hard
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DifficultyPicker
