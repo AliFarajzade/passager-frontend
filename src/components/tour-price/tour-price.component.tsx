18824e6c151b879902d6a3a7301db44f387cbcc9
Ali <alifarajzade1@yahoo.com>
2022-06-19 15:20:03 +0430

    Create tour price component.

---
+++ src/components/tour-price/tour-price.component.tsx
@@ -0,0 +1,28 @@
+import { FiBookmark } from 'react-icons/fi'
+
+interface IProps {
+    tourPrice: number
+}
+
+const TourPrice: React.FC<IProps> = ({ tourPrice }) => {
+    return (
+        <div className="p-4 border-[1px] border-lightGreenAlpha rounded-md w-full shadow-lg space-y-4">
+            <span className="text-4xl font-bold tracking-wide text-gradient">
+                ${tourPrice}
+            </span>
+            <div className="grid grid-cols-4 ">
+                <button className="btn btn-primary border-0 col-span-3 font-bold text-white text-lg capitalize btn-rgb">
+                    Book Now!
+                </button>
+                <button className="col-span-1 btn btn-primary btn-circle btn-outline justify-self-end border-2">
+                    <FiBookmark size="1.3em" />
+                </button>
+            </div>
+            <p className="text-sm text-gray-500 text-center">
+                You can cancel the booking 24 hours before the tour begins.
+            </p>
+        </div>
+    )
+}
+
+export default TourPrice
