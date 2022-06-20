ad53e46679452ec3a5cd715e5a3c6b94480875cf
Ali <alifarajzade1@yahoo.com>
2022-06-20 09:25:33 +0430

    Chagne some styles.

--- src/components/tour-guide-card/tour-guide-card.component.tsx
+++ src/components/tour-guide-card/tour-guide-card.component.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { FaInstagram } from 'react-icons/fa'
 
 interface IProps {
     tourGuide: {
@@ -28,11 +27,27 @@ const TourGuideCard: React.FC<IProps> = ({ tourGuide }) => {
                 <p className="text-gray-600 ">{tourGuide.description}</p>
             </div>
             <a href="https://instagram.com" target="_blank" rel="noreferrer">
-                <FaInstagram
-                    size="2.7em"
-                    className="text-gray-400 hover:text-gray-800"
-                    cursor="pointer"
-                />
+                <div className="btn-group">
+                    <button className="btn btn-primary btn-sm">Follow</button>{' '}
+                    <button
+                        aria-label="button component"
+                        className="btn btn-primary btn-square btn-sm"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="h-6 w-6 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
             </a>
         </div>
     )
