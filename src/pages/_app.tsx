d9ace39aaa02e0d5b6eb0247d541acd550e3f4e2
Ali <alifarajzade1@yahoo.com>
2022-06-21 15:47:12 +0430

    Add nprogress to app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import Layout from '../components/layout/layout.component'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const handleRouteStart = () => NProgress.start()
+        const handleRouteDone = () => NProgress.done()
+
+        Router.events.on('routeChangeStart', handleRouteStart)
+        Router.events.on('routeChangeComplete', handleRouteDone)
+        Router.events.on('routeChangeError', handleRouteDone)
+
+        return () => {
+            Router.events.off('routeChangeStart', handleRouteStart)
+            Router.events.off('routeChangeComplete', handleRouteDone)
+            Router.events.off('routeChangeError', handleRouteDone)
+        }
+    }, [])
+
     return (
         <RecoilRoot>
             <Layout>
