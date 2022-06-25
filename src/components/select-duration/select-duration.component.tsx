e46bfabbf5e3a1b37c98477c9643025b49221a97
Ali <alifarajzade1@yahoo.com>
2022-06-25 21:09:54 +0430

    Filter tours based on duration.

--- src/components/select-duration/select-duration.component.tsx
+++ src/components/select-duration/select-duration.component.tsx
@@ -1,4 +1,41 @@
+import { useRouter } from 'next/router'
+import { ChangeEvent, useEffect, useState } from 'react'
+
 const SelectDuration: React.FC = () => {
+    const [minMax, setMinMax] = useState<{
+        min: string | undefined
+        max: string | undefined
+    }>({
+        min: undefined,
+        max: undefined,
+    })
+
+    const router = useRouter()
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const min = event.target.dataset.min
+        const max = event.target.dataset.max
+
+        console.log({ min, max })
+
+        setMinMax({ min, max })
+    }
+
+    useEffect(() => {
+        if (!minMax.min && !minMax.max) return
+
+        const queryObj = { ...router.query }
+
+        if (minMax.min) queryObj['duration[gte]'] = minMax.min
+        if (minMax.max) queryObj['duration[lte]'] = minMax.max
+
+        router.push({
+            query: queryObj,
+        })
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [minMax.min, minMax.max])
+
     return (
         <div className="p-4">
             <h2 className="text-lg font-semibold mb-2">Duration</h2>
@@ -7,6 +44,8 @@ const SelectDuration: React.FC = () => {
                     type="radio"
                     name="radio-1"
                     className="radio radio-primary"
+                    data-max={2}
+                    onChange={handleChange}
                 />
                 Up to 2 days
             </div>
@@ -15,6 +54,9 @@ const SelectDuration: React.FC = () => {
                     type="radio"
                     name="radio-1"
                     className="radio radio-primary"
+                    data-max={4}
+                    data-min={3}
+                    onChange={handleChange}
                 />
                 Between 3 to 4 days
             </div>
@@ -24,6 +66,9 @@ const SelectDuration: React.FC = () => {
                     type="radio"
                     name="radio-1"
                     className="radio radio-primary"
+                    data-max={8}
+                    data-min={5}
+                    onChange={handleChange}
                 />
                 Between 5 to 8 days
             </div>
@@ -33,6 +78,8 @@ const SelectDuration: React.FC = () => {
                     type="radio"
                     name="radio-1"
                     className="radio radio-primary"
+                    data-min={9}
+                    onChange={handleChange}
                 />
                 More than 9 days
             </div>
