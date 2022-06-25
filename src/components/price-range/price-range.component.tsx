814eaff6751e821bf01571892db880d4665651f7
Ali <alifarajzade1@yahoo.com>
2022-06-25 20:48:29 +0430

    Filter tours based on price.

--- src/components/price-range/price-range.component.tsx
+++ src/components/price-range/price-range.component.tsx
@@ -1,7 +1,33 @@
-import { useState } from 'react'
+import debounce from 'lodash.debounce'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 const PriceRange: React.FC = () => {
     const [priceRange, setPriceRange] = useState<number>(999)
+
+    const router = useRouter()
+
+    const getToursBasedOnPrice = debounce(
+        () =>
+            router.push({
+                query: { ...router.query, 'price[lte]': priceRange },
+            }),
+        500
+    )
+
+    useEffect(() => {
+        getToursBasedOnPrice()
+
+        return () => getToursBasedOnPrice.cancel()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [priceRange])
+
+    useEffect(() => {
+        getToursBasedOnPrice.cancel()
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <div className="p-4 select-none">
             <h2 className="text-lg py-4 font-semibold ">Price</h2>
