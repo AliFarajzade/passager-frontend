8f21c7344876120fb19c607d48b18825ee87737f
Ali <alifarajzade1@yahoo.com>
2022-06-26 11:01:06 +0430

    Filter tours based on difficulty.

--- src/components/difficulty-picker/difficulty-picker.component.tsx
+++ src/components/difficulty-picker/difficulty-picker.component.tsx
@@ -1,28 +1,57 @@
+import { useRouter } from 'next/router'
+import { ChangeEvent, useEffect, useState } from 'react'
+
 const DifficultyPicker: React.FC = () => {
+    const [difficulty, setDifficulty] = useState<string | undefined>(undefined)
+
+    const handleChagne = (event: ChangeEvent<HTMLInputElement>) => {
+        const difficulty = event.target.dataset.difficulty as string
+        setDifficulty(difficulty)
+    }
+
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!difficulty) return
+
+        router.push({
+            query: { ...router.query, difficulty },
+        })
+    }, [difficulty])
+
     return (
         <div className="p-4">
             <h2 className="text-lg py-4 font-semibold mb-3">Difficulty</h2>
             <div className="flex flex-col gap-5">
                 <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
+                        onChange={handleChagne}
+                        name="radio-1"
+                        type="radio"
+                        className="radio radio-primary"
+                        data-difficulty="easy"
                     />
                     Easy
                 </div>
                 <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
+                        onChange={handleChagne}
+                        name="radio-1"
+                        type="radio"
+                        className="radio radio-primary"
+                        data-difficulty="medium"
                     />
                     Medium
                 </div>
                 <div className="flex items-center gap-4 px-4 py-0 cursor-pointer">
                     <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
+                        onChange={handleChagne}
+                        name="radio-1"
+                        type="radio"
+                        className="radio radio-primary"
+                        data-difficulty="difficult"
                     />
-                    Hard
+                    Difficult
                 </div>
             </div>
         </div>
