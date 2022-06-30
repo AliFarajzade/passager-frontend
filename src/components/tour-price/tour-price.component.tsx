b2ec0c6e9bf8672ab5f4f0a808affd6e11f2582a
Ali <alifarajzade1@yahoo.com>
2022-06-30 11:07:55 +0430

    Add booking functionality.

--- src/components/tour-price/tour-price.component.tsx
+++ src/components/tour-price/tour-price.component.tsx
@@ -1,15 +1,38 @@
+import { useSetRecoilState } from 'recoil'
+import useGetMe from '../../hooks/use-get-me.hook'
+import authModalStateAtom from '../../recoil/atoms/auth-modal.atom'
+
 interface IProps {
     tourPrice: number
 }
 
 const TourPrice: React.FC<IProps> = ({ tourPrice }) => {
+    const [userState] = useGetMe()
+    const setAuthModalState = useSetRecoilState(authModalStateAtom)
+
+    const handleBook = () => {
+        if (!userState.user)
+            return setAuthModalState(prevState => ({
+                ...prevState,
+                open: true,
+                view: 'signIn',
+            }))
+
+        alert(
+            'You have booked the tour!\nThis is fake abviously and to implement this I needed to use stripe or some payment solution which is not implemented.'
+        )
+    }
+
     return (
         <div className="p-4 border-[1px] border-lightGreenAlpha rounded-md w-full shadow-lg space-y-4 bg-white">
             <span className="text-4xl font-bold tracking-wide text-gradient">
                 ${tourPrice}
             </span>
             <div className="flex gap-3">
-                <button className="btn btn-primary border-0 col-span-3 font-bold text-white text-lg capitalize btn-rgb flex-1">
+                <button
+                    className="btn btn-primary border-0 col-span-3 font-bold text-white text-lg capitalize btn-rgb flex-1"
+                    onClick={handleBook}
+                >
                     Book Now!
                 </button>
             </div>
