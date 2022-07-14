6a2ab28ead5727583d97bc529252a8d7d2281955
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-14 10:24:05 +0430

    Wake up Heroku server at initialize.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,16 @@ import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { RecoilRoot } from 'recoil'
 import Layout from '../components/layout/layout.component'
+import axiosInstance from '../helpers/axios-instance.helper'
 import { printASCII } from '../helpers/print-ascii.helper'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
+        // Print art in console
+        printASCII()
+
+        // NProgress
         const handleRouteStart = () => NProgress.start()
         const handleRouteDone = () => NProgress.done()
 
@@ -17,7 +22,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         Router.events.on('routeChangeComplete', handleRouteDone)
         Router.events.on('routeChangeError', handleRouteDone)
 
-        printASCII()
+        // Waking Heroku server up.
+        ;(async () => {
+            await axiosInstance.get('/tours').then(res => res)
+        })()
 
         return () => {
             Router.events.off('routeChangeStart', handleRouteStart)
