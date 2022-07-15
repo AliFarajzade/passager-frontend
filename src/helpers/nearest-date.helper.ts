becdf9aa27d8eebf249d6a85af8257d6e82466d7
Ali Farajzade <alifarajzade1@yahoo.com>
2022-07-15 12:43:34 +0430

    Add helper function for getting the next month.

--- src/helpers/nearest-date.helper.ts
+++ src/helpers/nearest-date.helper.ts
@@ -1,2 +1,8 @@
 export const getNearestDate = (dates: string[]) =>
     dates.filter(date => new Date(date).getTime() - Date.now() > 0).sort()[0]
+
+export const nextMonth = (): Date => {
+    const now = new Date()
+    if (now.getMonth() === 11) return new Date(now.getFullYear() + 1, 0, 1)
+    else return new Date(now.getFullYear(), now.getMonth() + 1, 1)
+}
